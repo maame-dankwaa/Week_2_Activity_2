d258be2461be8570e88ed2beec55bef27ea125e1
maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>
2025-09-25 17:08:00 +0000

    Update core.php

--- settings/core.php
+++ settings/core.php
@@ -1,15 +1,21 @@
 // Settings/core.php
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
+}
 
 
 //for header redirection
 ob_start();
 
-//funtion to check for login
-if (!isset($_SESSION['id'])) {
-    header("Location: ../Login/login_register.php");
-    exit;
+//function to check if a user is logged in
+function isUserLoggedIn() {
+	return (session_status() === PHP_SESSION_ACTIVE) && isset($_SESSION['id']);
+}
+
+//function to check if user has administrative privileges
+function isAdmin() {
+	return isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin';
 }
 
 
